5fbbb021981ff52a4621246d9568ca14dbead52d
dshmatkov <client.pas.95@gmail.com>
2017-05-20 17:32:50 +0300

    Added note.

---
+++ note/clear.sh
@@ -0,0 +1,8 @@
+find . -name "*.aux" -type f -delete
+find . -name "*.bbl" -type f -delete
+find . -name "*.blg" -type f -delete
+find . -name "*.log" -type f -delete
+find . -name "*.out" -type f -delete
+find . -name "*.toc" -type f -delete
+find . -name "*.gz" -type f -delete
+find . -name "*.fdb_latexmk" -type f -delete
