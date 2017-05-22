958f4b20b2150fe5189af30595e574850e2f20d4
dshmatkov <client.pas.95@gmail.com>
2017-05-22 04:04:11 +0300

    UPD

--- clear.sh
+++ clear.sh
@@ -6,3 +6,5 @@ find . -name "*.out" -type f -delete
 find . -name "*.toc" -type f -delete
 find . -name "*.gz" -type f -delete
 find . -name "*.fdb_latexmk" -type f -delete
+find . -name "*.fls" -type f -delete
+find . -name "*.suplime-project" -type f -delete
